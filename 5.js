5dea0df66cd50550e4ceb58573f0aa2f105bfcbc
kaldne <tseringkalden1964@gmail.com>
2020-06-03 12:04:23 -0700

    today algo

--- 5.js
+++ 5.js
@@ -9,31 +9,32 @@
 //   });
 //   return num[0];
 // }
-function cakes(recipe, available) {
-  var numCakes = [];
+function cakes(recipe, ingredients) {
+  const recipeKeys = {};
+  let cakes = null;
 
-  for (var key in recipe) {
-    if (recipe.hasOwnProperty(key)) {
-      if (key in available) {
-        numCakes.push(Math.floor(available[key] / recipe[key]));
-      } else {
-        return 0;
-      }
-    }
+  for (
+    let i = 0;
+    i < recipeKeys.length &&
+    (ingredients[recipeKeys[i]] || (cakes = 0 && false));
+    i++
+  ) {
+    const amount = Math.floor(
+      ingredients[recipeKeys[i]] / recipe[recipeKeys[i]]
+    );
+
+    if (cakes === null || amount < cakes) cakes = amount;
   }
 
-  return Math.min.apply(null, numCakes);
+  return cakes || 0;
 }
 
-  var recipe, available;
+var recipe, available;
 
- 
-    recipe = {flour: 500, sugar: 200, eggs: 1};
-    available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
-    console.log(cakes(recipe, available), 2, 'Wrong result for example #1');
+recipe = {flour: 500, sugar: 200, eggs: 1};
+available = {flour: 1200, sugar: 1200, eggs: 5, milk: 200};
+console.log(cakes(recipe, available), 2, 'Wrong result for example #1');
 
-    recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
-    available = {sugar: 500, flour: 2000, milk: 2000};
-    console.log(cakes(recipe, available), 0, 'Wrong result for example #2');
-
-
+recipe = {apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100};
+available = {sugar: 500, flour: 2000, milk: 2000};
+console.log(cakes(recipe, available), 0, 'Wrong result for example #2');
