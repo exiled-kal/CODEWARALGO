ee92582fa91d4c4efc5e19eba44ec20e9bc5a66c
Tsering_kalden <49007283+exiled-kal@users.noreply.github.com>
2021-04-08 12:40:02 -0700

    Update containclosenum.js

--- containclosenum.js
+++ containclosenum.js
@@ -30,10 +30,8 @@
 // [output] boolean
 
 function containsCloseNums(nums, k) {
-  
-
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] == nums[j]) if (Math.abs(i - j) <= k) return true;
     }
   }
