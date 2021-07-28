fedf5ba3f1a99c40873b40938ad0569e091b2636
kalden <tseringkalden1964@gmail.com>
2021-07-28 10:59:54 -0700

    iscrypt

--- returnsum2num.js
+++ returnsum2num.js
@@ -2,3 +2,21 @@ function add(param1, param2) {
   if (typeof param1 == "number" && typeof param2 == "number")
     return param1 + param2;
 }
+
+function isCryptSolution(crypt, solution) {
+  let hasLeadingZeros = false;
+  const decrypted = crypt.map(word => {
+      const number = word.split('').map(letter => {
+          return solution.find(key => key[0] === letter)[1];
+      }).join('');
+      
+      if (number.startsWith('0') && number.length > 1) {
+          hasLeadingZeros = true;
+      }
+      
+      return Number.parseInt(number);
+  });
+  
+  
+  return !hasLeadingZeros && decrypted[0] + decrypted[1] === decrypted[2];
+}
